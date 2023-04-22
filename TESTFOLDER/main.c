1509c6054e232e3220b74047f74b85c1b4fd7ab7
BrainBoxTayo <temitayo.akanbi-bello@stu.cu.edu.ng>
2023-04-22 15:40:43 +0100

    Edgecases

--- TESTFOLDER/main.c
+++ TESTFOLDER/main.c
@@ -4,9 +4,9 @@ int main()
 {
 	char *str = "temitayo";
 	char alpha = 'c';
-	int number = 10;
+	int number = -10;
 	int number2 = 0x64;
-	int storeNum = _printf("This is a %s, %c,\n %d, %i", str, alpha, number, number2);
+	int storeNum = _printf("This is a %s, %c, %d, %i\n", str, alpha, number, number2);
 
 	
 	_printf("%d\n", storeNum);
