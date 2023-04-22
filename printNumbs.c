9d84a13d881663b8f294869443de0e09f4272832
BrainBoxTayo <temitayo.akanbi-bello@stu.cu.edu.ng>
2023-04-22 12:51:52 +0100

    added a number printing function

---
+++ printNumbs.c
@@ -0,0 +1,27 @@
+#include"main.h"
+/**
+  * print_number - prints a number
+  * @n: number to be printed
+  * Return: nothing
+  */
+
+void print_number(int n)
+{
+	unsigned int num = 0;
+
+	if (n < 0)
+	{
+		_putchar(45);
+		num = -n;
+	}
+	else
+		num = n;
+	/*remove last digit and recur - why I don't know yet*/
+	/* I think I understand the recursion*/
+	/*if the last digit is removed everytime the program*/
+	/*does it again over and over until it reaches first digit*/
+	if (num / 10)
+		print_number(num / 10);
+	/*print last digit*/
+	_putchar((num % 10) + 48);
+}
