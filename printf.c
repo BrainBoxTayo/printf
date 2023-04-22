f961aedc8d88e5b275310ed9268ebd9ae285b7a3
BrainBoxTayo <temitayo.akanbi-bello@stu.cu.edu.ng>
2023-04-22 16:04:42 +0100

    fixed git issues

---
+++ printf.c
@@ -0,0 +1,44 @@
+#include <stdarg.h>
+#include "main.h"
+/**
+ * _printf - formatted printing
+ * @format: string to be printed {"this is an integer %d"}
+ * Return: the number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+	va_list nextVar;
+	int index = 0, count = 0;
+
+	va_start(nextVar, format);
+	while (format[index])
+	{
+		/*this is for string*/
+		if (format[index] == '%')
+		{
+			index++;
+			switch (format[index])
+			{
+				case ('s'):
+					count += print_string(nextVar);
+					break;
+				case ('c'):
+					count += print_char(nextVar);
+					break;
+				case ('d'):
+				case ('i'):
+					count += print_number(nextVar);
+					break;
+				default:
+					break;
+			}
+		}
+		else
+		{
+			count += _putchar(format[index]);
+		}
+		index++;
+	}
+	va_end(nextVar);
+	return (count);
+}
