de9266c62663fd899fd58f78156750455d7c5a8c
BrainBoxTayo <temitayo.akanbi-bello@stu.cu.edu.ng>
2023-04-22 15:25:11 +0100

    more edge cases

--- printf.c
+++ printf.c
@@ -13,6 +13,8 @@ int _printf(const char *format, ...)
 	va_start(nextVar, format);
 	if (!format || (format[0] == '%' && !format[1]))
 		return (-1);
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
 	while (format[index])
 	{
 		/*this is for string*/
