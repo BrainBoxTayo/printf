cda68d1e05f092470d562ce7186ad90d8b2071cf
BrainBoxTayo <temitayo.akanbi-bello@stu.cu.edu.ng>
2023-04-22 10:34:52 +0100

    Made template for printf

---
+++ printf.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdarg.h>
+/**
+  * _printf - formatted printing
+  * @format: string to be printed {"this is an integer %d"}
+  * Return: the number of characters printed
+  */
+
+int _printf(const char *format, ...)
+{
+	int form_size;
+
+	for (form_size = 0; format[form_size] != '\0'; form_size++)
+		;
+
+	/*"this is a %c boy"*/
+
+	/**/
+	while (format)
+	{
+
+	}
+
+
+	return (form_size);
+}
