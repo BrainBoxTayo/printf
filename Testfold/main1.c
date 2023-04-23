be336ceebadc81b842a7b3953dbe009bd616019c
BrainBoxTayo <temitayo.akanbi-bello@stu.cu.edu.ng>
2023-04-23 15:56:38 +0100

    fixed when a %c23 is passed remove c and replace with space

--- Testfold/main1.c
+++ Testfold/main1.c
@@ -5,12 +5,14 @@ int main(__attribute__((unused))int argc, __attribute__((unused))char **argv)
 {
 	int intVar = 0x123;
 	char *stringVar = "String 0x123";
+	int counter_org, counter_dop = 0;
 
-	_printf("This is an int %i and also %d % t\n", intVar, intVar);
-	 printf("This is an int %i and also %d % t\n", intVar, intVar);
+	counter_dop = _printf("This is an int %i and also %d % t\n", intVar, intVar);
+	 counter_org = printf("This is an int %i and also %d % t\n", intVar, intVar);
 
 	_printf("This is a %s % t12\n", stringVar);
 	 printf("This is a %s % t12\n", stringVar);
-	
+
+	 printf("%d  %d", counter_org, counter_dop);
 	return (argc);
 }
